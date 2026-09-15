fac3834cc8789510bc72cbba9150224ae8cb1809
“marwa-hodeib” <marwahodeibdeveloper@gmail.com>
2026-09-15 13:06:49 +0300

    Angular crime done

--- src/app/pages/angular-crimes/angular-crimes.component.ts
+++ src/app/pages/angular-crimes/angular-crimes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-angular-crimes',
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
   templateUrl: './angular-crimes.component.html',
   styleUrl: './angular-crimes.component.scss',
 })
-export class AngularCrimesComponent {}
+export class AngularCrimesComponent {
+  status = signal('Offline');
+  isOnline = signal(false);
+  showError = signal(false);
+  shakeError = signal(false);
+  activeTab = signal<'vanilla' | 'angular'>('vanilla');
+
+  // Vanilla JS
+  goOnline() {
+    const status = document.querySelector('#status');
+
+    if (status) {
+      if (this.isOnline()) {
+        status.textContent = 'Offline';
+        status.classList.remove('text-green-500');
+        status.classList.add('text-red-500');
+      } else {
+        status.textContent = 'Online';
+        status.classList.remove('text-red-500');
+        status.classList.add('text-green-500');
+      }
+    }
+
+    this.isOnline.set(!this.isOnline());
+
+    this.showError.set(true);
+
+    setTimeout(() => {
+      this.shakeError.set(true);
+
+      setTimeout(() => {
+        this.shakeError.set(false);
+      }, 400);
+    }, 50);
+  }
+
+  //  Angular
+  goOnlineAngular() {
+    const newState = !this.isOnline();
+
+    this.isOnline.set(newState);
+    this.status.set(newState ? 'Online' : 'Offline');
+  }
+
+  //Tab change logic//
+  selectTab(tab: 'vanilla' | 'angular') {
+    this.activeTab.set(tab);
+  }
+}
